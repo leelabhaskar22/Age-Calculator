8be61190d369dbdf2c24710a3672bae2398b40ac
leelabhaskar22 <107635790+leelabhaskar22@users.noreply.github.com>
2023-05-13 08:16:30 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -29,7 +29,7 @@ var date = document.getElementById("date").value;
 const leapYears = LeapYears(year,currentYear);
 leapcount = leapYears.length;
     var age = (currentYear - year) - 1;
-    const month1 = (12 - monthh) ;
+    const month1 = (12 - (monthh + 1) );
     const month = ((12 * age) + currentMonth) + month1;
     const day1 = ((18-leapcount)*365)+(366*leapcount)
     birthyear_days=0;
@@ -49,7 +49,7 @@ leapcount = leapYears.length;
     const second =(min * 60);
     tolmon=month ;
     document.getElementById("text").innerHTML = "<span style=font-size:20px ;    display: inline-block; width: 100px;text-align: right;   padding-right: 5%;>Your age in </span>";
-    document.getElementById("yearr").innerHTML = `<span style="color: #db3434;">Years</span>  ${age}`;
+    document.getElementById("yearr").innerHTML = `<span style="color: #db3434;">Years</span> : ${age}`;
     document.getElementById("mon").innerHTML = `<span style="color: #db3434;">Months</span> :  ${tolmon}`
     document.getElementById("dayy").innerHTML = `<span style="color: #db3434;">Days</span> :   ${day}`;
     document.getElementById("hour").innerHTML = `<span style="color: #db3434;">Hours</span> :${hours}`;
